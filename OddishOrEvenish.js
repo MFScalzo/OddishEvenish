e099c0a087adc3c90316a4d222f3ca62516fab3b
Matt Scalzo <matthewfscalzo@gmail.com>
2022-07-26 13:26:13 -0400

    Implement Challenge Code

---
+++ OddishOrEvenish.js
@@ -0,0 +1,18 @@
+function oddishOrEvenish(n) {
+    var sum = 0;
+
+    while (n > 0) {
+        sum = sum + (n % 10)
+        n = Math.floor(n / 10)
+    }
+
+    if (sum % 2 == 0) {
+        return "Evenish"
+    }
+
+    return "Oddish"
+}
+
+console.log(oddishOrEvenish(43))
+console.log(oddishOrEvenish(373))
+console.log(oddishOrEvenish(4433))
